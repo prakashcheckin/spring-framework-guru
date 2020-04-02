2d56dd851b30675b5b5441904461f9cfae2f48ab
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-03 12:02:15 +1300

    new java project

---
+++ src/main/java/com/example/springframeworkguru/SpringFrameworkGuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframeworkguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkGuruApplication.class, args);
+	}
+
+}
